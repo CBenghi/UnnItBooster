9f359547f4cc53c9c791abaae18c8ca76f7b1f64
Claudio Benghi <claudio.benghi@gmail.com>
2018-10-03 14:40:28 +0200

    Added (not working) ams campus functionality.

--- UnnITBooster/Calendar/frmGoogleCalendar.Designer.cs
+++ UnnITBooster/Calendar/frmGoogleCalendar.Designer.cs
@@ -39,40 +39,36 @@
             // 
             // dtWeek1
             // 
-            this.dtWeek1.Location = new System.Drawing.Point(119, 327);
-            this.dtWeek1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtWeek1.Location = new System.Drawing.Point(89, 266);
             this.dtWeek1.Name = "dtWeek1";
-            this.dtWeek1.Size = new System.Drawing.Size(335, 22);
+            this.dtWeek1.Size = new System.Drawing.Size(252, 20);
             this.dtWeek1.TabIndex = 0;
-            this.dtWeek1.Value = new System.DateTime(2012, 9, 24, 0, 0, 0, 0);
+            this.dtWeek1.Value = new System.DateTime(2018, 9, 24, 0, 0, 0, 0);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 329);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(15, 267);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Day 1";
             // 
             // txtSchedule
             // 
             this.txtSchedule.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSchedule.Location = new System.Drawing.Point(24, 48);
-            this.txtSchedule.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSchedule.Location = new System.Drawing.Point(18, 39);
             this.txtSchedule.Multiline = true;
             this.txtSchedule.Name = "txtSchedule";
-            this.txtSchedule.Size = new System.Drawing.Size(429, 262);
+            this.txtSchedule.Size = new System.Drawing.Size(323, 214);
             this.txtSchedule.TabIndex = 2;
-            this.txtSchedule.Text = "weeks 1,3,5,12\r\n3-15-1-Be1178 Lecture-EBB003\r\n3-17-1-Be1178 Seminar-WJC207";
+            this.txtSchedule.Text = "weeks 5,6\r\n2-1-2-Be1178 Lecture-EBB003\r\n3-3-2-Be1178 Seminar-WJC207";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(245, 425);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(184, 345);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 28);
+            this.button1.Size = new System.Drawing.Size(157, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "Go";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,10 +77,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 362);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(15, 294);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.Size = new System.Drawing.Size(68, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "DST change";
             // 
@@ -92,31 +87,29 @@
             // 
             this.dstchange.CustomFormat = "dd MMMM yyyy HH mm";
             this.dstchange.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dstchange.Location = new System.Drawing.Point(119, 359);
-            this.dstchange.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dstchange.Location = new System.Drawing.Point(89, 292);
             this.dstchange.Name = "dstchange";
-            this.dstchange.Size = new System.Drawing.Size(335, 22);
+            this.dstchange.Size = new System.Drawing.Size(252, 20);
             this.dstchange.TabIndex = 7;
-            this.dstchange.Value = new System.DateTime(2012, 10, 28, 0, 0, 0, 0);
+            this.dstchange.Value = new System.DateTime(2018, 10, 28, 0, 0, 0, 0);
             // 
             // chkIsSummer
             // 
             this.chkIsSummer.AutoSize = true;
             this.chkIsSummer.Checked = true;
             this.chkIsSummer.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkIsSummer.Location = new System.Drawing.Point(119, 391);
-            this.chkIsSummer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkIsSummer.Location = new System.Drawing.Point(89, 318);
             this.chkIsSummer.Name = "chkIsSummer";
-            this.chkIsSummer.Size = new System.Drawing.Size(162, 21);
+            this.chkIsSummer.Size = new System.Drawing.Size(123, 17);
             this.chkIsSummer.TabIndex = 9;
             this.chkIsSummer.Text = "It\'s currently Summer";
             this.chkIsSummer.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 489);
+            this.ClientSize = new System.Drawing.Size(363, 397);
             this.Controls.Add(this.chkIsSummer);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dstchange);
@@ -124,10 +117,8 @@
             this.Controls.Add(this.txtSchedule);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtWeek1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- UnnITBooster/Calendar/frmGoogleCalendar.cs
+++ UnnITBooster/Calendar/frmGoogleCalendar.cs
@@ -41,7 +41,8 @@ namespace StudentsFetcher
                     foreach (string week in weeks)
                     {
                         int intWeek = Convert.ToInt32(week);
-
+                        if (intWeek > 4)
+                            intWeek++;
                         int iCursor = 0;
                         string[] field = appointment.Split('-');
                         int iDaysAddToSunday = Convert.ToInt32(field[iCursor++]);
@@ -49,16 +50,9 @@ namespace StudentsFetcher
                         int iHours = Convert.ToInt32(field[iCursor++]);
                         string sDesc = field[iCursor++];
                         string sLoc = field[iCursor++];
-
                         DateTime EventDay = dtWeek1.Value.AddDays(7 * (intWeek - 1) + iDaysAddToSunday - 1);
-
-                        if (EventDay < dstchange.Value && chkIsSummer.Checked)
-                            iHour = iHour - 1;
-                        else if (EventDay > dstchange.Value && !chkIsSummer.Checked)
-                            iHour = iHour - 1;
-
                         writeFile.Write(
-                            Addevent(EventDay, iHour, iHours, sLoc, sDesc, "week " + intWeek)
+                            AddeventAUAS(EventDay, iHour, iHours, sLoc, sDesc, "week " + week)
                             );
                     }
                 }
@@ -67,6 +61,15 @@ namespace StudentsFetcher
             writeFile.Flush();
             writeFile.Close();
             writeFile = null;
+        }
+
+        private bool needRemoveHour(DateTime eventDay)
+        {
+            if (eventDay < dstchange.Value && chkIsSummer.Checked)
+                return true;
+            else if (eventDay > dstchange.Value && !chkIsSummer.Checked)
+                return true;
+            return false;
         }
 
         private void Init(TextWriter sb)
@@ -103,9 +106,49 @@ namespace StudentsFetcher
             return sb.ToString();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        string AddeventAUAS(DateTime day, int timeHour, int durationHours, string location, string name, string addtext)
         {
+            day = day.AddHours(9);
+            day = day.AddMinutes(20);
 
+            bool needRemove = needRemoveHour(day);
+            if (needRemove)
+                day = day.AddHours(-1);
+
+            // ams time
+            day = day.AddHours(-1);
+
+            while (--timeHour > 0)
+            {
+                day = day.AddMinutes(50);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("BEGIN:VEVENT\r\n");
+            sb.Append($"DTSTART:{DropDayTime(day)}\r\n");
+            for (int i = 0; i < durationHours; i++)
+            {
+                day = day.AddMinutes(50);
+            }
+
+            sb.Append($"DTEND:{DropDayTime(day)}\r\n");
+            //sb.AppendFormat("DTSTAMP:20100923T080655Z\r\n");
+            //sb.AppendLine("UID:35o3qebq626coohim9ctp78508@google.com");
+            //sb.AppendFormat("CREATED:20100916T155837Z\r\n");
+            sb.Append($"DESCRIPTION:Automated lecture calendar " + addtext + "\r\n");
+            //sb.AppendLine("LAST-MODIFIED:20100923T080639Z");
+            sb.Append($"LOCATION:" + location + "\r\n");
+            sb.Append($"SEQUENCE:0\r\n");
+            sb.Append($"STATUS:CONFIRMED\r\n");
+            sb.Append($"SUMMARY:" + name + "\r\n");
+            sb.Append($"TRANSP:OPAQUE\r\n");
+            sb.Append($"END:VEVENT\r\n");
+            return sb.ToString();
+        }
+
+        private string DropDayTime(DateTime day)
+        {
+            return $"{day:yyyyMMdd}T{day.Hour:00}{day.Minute:00}00Z";
         }
     }
 }
