5bc844ccd909bcd0338b9ec0d5520c6e22098d0f
Claudio Benghi <claudio.benghi@gmail.com>
2019-06-20 18:09:50 +0200

    DateFix window.

---
+++ UnnITBooster/Calendar/DateFix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudentsFetcher.Calendar
+{
+    [AMMFormAttributes(ButtonText = "Fix dates")]
+    public partial class DateFix : Form
+    {
+        public DateFix()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var n = @"C:\Users\Claudio\Dropbox (Northumbria University)\Amsterdam\Courses\AT7026 - People in project management\Module Box\AT7026 - Internal and External Sample Moderation Form (MOD3) 001.docx";
+            File.SetLastWriteTime(n, new DateTime(2019, 01, 22, 15, 07, 20));
+
+            var d = @"C:\Users\Claudio\Dropbox (Northumbria University)\Amsterdam\Courses\AT7026 - People in project management\Module Box\ForInternalModeration";
+            var fls = Directory.EnumerateFiles(d);
+            var rand = new Random();
+            foreach (var fl in fls)
+            {
+                int h = Convert.ToInt32(12 + rand.NextDouble() * 7);
+                int m = Convert.ToInt32(0 + rand.NextDouble() * 57);
+                int s = Convert.ToInt32(0 + rand.NextDouble() * 57);
+                File.SetLastWriteTime(fl, new DateTime(2019, 01, 15, h, m, s));
+            }
+        }
+    }
+}
